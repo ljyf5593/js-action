fd3984d9752263658fa0cd62e9cda0377216050b
Liu Jie <ljyf5593@gmail.com>
2013-08-12 11:57:17 +0800

    添加滚动到相应元素的js效果

---
+++ common.js
@@ -0,0 +1,13 @@
+/* 
+    ----
+    公共常用JS效果
+    ----
+*/
+
+/* --- 浏览器滚动到相应元素位置（兼容firefox） --- */
+var offsetTop = $(element).position().top;
+// chrome ie
+$('body').animate({ scrollTop: offsetTop}, 200);
+// firefox
+document.documentElement.scrollTop = offsetTop;
+/* --- end --- */
